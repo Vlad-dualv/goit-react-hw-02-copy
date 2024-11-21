6ed94a7f7c22434b7a964d87d7cc301b3dc0a2ed
Vlad <vladswork95@gmail.com>
2024-11-21 17:18:02 +0000

    Ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,62 @@
-export default function App() {}
+import Description from './Description/Description';
+import Options from './Options/Options';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(() => {
+    const savedFeedback = window.localStorage.getItem('savedFeedback');
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('savedFeedback', JSON.stringify(count));
+  }, [count]);
+
+  const updateFeedback = feedbackType => {
+    setCount(prevCount => ({
+      ...prevCount,
+      [feedbackType]: prevCount[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setCount({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = count.good + count.neutral + count.bad;
+  const positiveFeedback = Math.round((count.good / totalFeedback) * 100);
+
+  return (
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          values={{
+            good: count.good,
+            neutral: count.neutral,
+            bad: count.bad,
+            total: totalFeedback,
+            positive: positiveFeedback,
+          }}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import css from './Feedback.module.css';
+
+export default function Feedback({
+  values: { good, neutral, bad, total, positive },
+}) {
+  return (
+    <div className={css.container}>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive: {positive}%</p>
+    </div>
+  );
+}
